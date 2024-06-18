7bb627b21eee907b9ed50336f302cc5caf847fbc
KubilayDekin <kubilaydekin@gmail.com>
2024-06-18 17:49:59 +0300

    Dynamic Environment Loader (wip)

---
+++ Assets/_Project/Scripts/DynamicEnvironmentLoader/DynamicEnvironmentLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.DynamicEnvironmentLoader
+{
+	public class DynamicEnvironmentLoader : MonoBehaviour
+	{
+		private void Awake()
+		{
+			
+		}
+	}
+}
